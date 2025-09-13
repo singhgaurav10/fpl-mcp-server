8d6546aa93331426c63033f142c23290f236db46
singhgaurav10 <singhgauravbel@gmail.com>
2025-09-13 15:28:11 +0530

    First Commit. Spring Initializer

---
+++ src/main/java/com/gauravs/fpl_mcp_server/FplMcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.gauravs.fpl_mcp_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FplMcpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FplMcpServerApplication.class, args);
+	}
+
+}
